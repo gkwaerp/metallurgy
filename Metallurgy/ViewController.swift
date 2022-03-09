314fa579849f834893b759f5e327e2e035a8285c
Geir-Kåre S. Wærp <gkwaerp@gmail.com>
2022-03-09 20:52:03 +0100

    Initial project commit

---
+++ Metallurgy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metallurgy
+//
+//  Created by Geir-Kåre S. Wærp on 09/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
